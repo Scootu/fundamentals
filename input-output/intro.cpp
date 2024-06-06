fa1d1ceea4ce763b33c1550e2b664138a35d7826
anesHamdaoui <bacetd2004@gmail.com>
2024-06-06 10:32:19 +0100

    first commit

---
+++ input-output/intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sample ;
+    cout << "Entre your text to be deployed " << endl ;
+	getline(cin,sample);
+	cout << sample << " - A computer science portal for geeks";
+
+	return 0;
+}
